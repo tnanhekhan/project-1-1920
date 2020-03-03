5f129cc8af9baf4d833879e8b6bc82f84006c68b
tnanhekhan <1631995@gmail.com>
2020-03-03 16:16:21 +0100

    paginated call + nicer structure

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,27 @@
 import {Api} from "./modules/data/api/api.mjs"
+import {BookRepository} from "./modules/data/repo/book-repository.mjs";
 
-const endpoint = "/search/?";
-const query = "q=guiness";
-const api = new Api();
+const query = "boek";
+const bookRepository = new BookRepository();
+let currentPage = 1;
 
-api.get(endpoint, query)
-    .then(res => {
-        let books = res.results;
-        console.log(books)
-    });
+document.getElementById("callTest").onclick = () => {
+    printBooks();
+};
+
+function printBooks() {
+    bookRepository.getBooks(query, currentPage)
+        .then(res => {
+            let books = res.results;
+            currentPage = parseInt(res.meta["current-page"]) + 1;
+            let frablList = books.map(book => {
+                return book.frabl.value
+            });
+            console.log(books);
+            // bookRepository.getBookDetails(frablList[18])
+            //     .then(results => {
+            //         console.log(results)
+            //     })
+        });
+}
+
