8b0c2fd4c4f92c248f0b77ee9b11bb4caa9017b1
tnanhekhan <1631995@gmail.com>
2020-03-04 14:57:54 +0100

    added router

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,2 @@
-import { Api } from "./modules/data/api/api.mjs";
-import { BookRepository } from "./modules/data/repo/book-repository.mjs";
+import "./modules/ui/router.mjs";
 
-const query = "boek";
-const bookRepository = new BookRepository();
-let currentPage = 1;
-
-document.getElementById("search").onclick = () => {
-  printBooks();
-};
-
-function printBooks() {
-  bookRepository.getBooks(query, currentPage).then(res => {
-    let books = res.results;
-    currentPage = parseInt(res.meta["current-page"]) + 1;
-    let frablList = books.map(book => {
-      return book.frabl.value;
-    });
-    console.log(books);
-    // bookRepository.getBookDetails(frablList[18])
-    //     .then(results => {
-    //         console.log(results)
-    //     })
-  });
-}
--- src/js/modules/data/repo/book-repository.mjs
+++ src/js/modules/data/repo/book-repository.mjs
@@ -5,13 +5,24 @@ export class BookRepository {
         this.api = api;
     }
 
+    getYouthBooks(query, page) {
+        const endpoint = "/search/?";
+        const sisoFilter = `q=(siso:"J*")`;
+        const resultPage = `&page=${page}`;
+        const facets = `&facet=Language(dut)&facet=Type(book)&facet=topic(${query})&facet=doelgroep(ageYouth)&refine=true`;
+        return this.api.get(endpoint, sisoFilter + resultPage + facets);
+    }
+
     getBooks(query, page) {
         const endpoint = "/search/?";
-        return this.api.get(endpoint, `q=${query}&page=${page}`);
+        const sisoFilter = `q=(siso:"J*")`;
+        const resultPage = `&page=${page}`;
+        const facets = `&facet=Language(dut)&facet=Type(book)&facet=topic(${query})&refine=true`;
+        return this.api.get(endpoint, sisoFilter + resultPage + facets);
     }
 
     getBookDetails(ids) {
         const endpoint = "/details/?";
-        return this.api.get(endpoint, `frabl=${ids}&librarian=true`);
+        return this.api.get(endpoint, `frabl=${ids}&detaillevel=extended`);
     }
 }
