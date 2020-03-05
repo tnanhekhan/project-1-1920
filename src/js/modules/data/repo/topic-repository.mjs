7ef984331bd199f5d113fe33b1c0a6e9c8815232
tnanhekhan <1631995@gmail.com>
2020-03-06 00:53:54 +0100

    refactor and cleanup

---
+++ src/js/modules/data/repo/topic-repository.mjs
@@ -0,0 +1,20 @@
+export class TopicRepository {
+    getGroups() {
+        return ["Groep 4", "Groep 5", "Groep 6", "Groep 7", "Groep 8"];
+    }
+
+    getTopics() {
+        return [
+            ["Dieren", "Honden", "Katten", "Paarden", "Dinosaurussen", "Vogels", "Huisdieren", "Wilde dieren", "Dolfijnen", "Zindelijkheid", "Insecten"],
+            ["Seizoenen", "Lente", "Zomer", "Herfst", "Winter", "jaargetijden", "Pasen"],
+            ["Landen", "Nederland", "Frankrijk", "Amsterdam", "Verenigde Staten", "Afrika", "Suriname", "New York (stad)"],
+            ["Geschiedenis", "Middeleeuwen", "17e eeuw", "19e eeuw", "Toekomst", "Egyptische Oudheid", "Prehistorie", "Jodenvervolging"],
+            ["Sport", "Voetbal"],
+            ["Mode", "Kleding", "Winkelen"],
+            ["Natuur", "Aarde", "Natuurwetenschappen", "Ruimtevaart"],
+            ["Reizen"],
+            ["Eten"],
+            ["Muziek"]
+        ];
+    }
+}
